086a695bd6859ad48eb60e934cf0fcf2d6154057
martijnv <martijnvoordouw@gmail.com>
2024-05-31 18:00:00 +0200

    Remade symbol layer, caps replaced by delete on base layer

--- keymap.c
+++ keymap.c
@@ -11,8 +11,8 @@
 #include QMK_KEYBOARD_H
 
 enum porcupine_layers {
-    _QWERTY,
     _COLMAK_DH,
+    _QWERTY,
     _SYMBOL,
     _NAVFUN,
 };
@@ -39,6 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   │ BACKSPC │ SYMBOL  │ NAVFUN  │  ALT    ││  LCTRL  │ LSHIFT  │ SPACE   │ LMETA   │
                                   └─────────┴─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┴─────────┘                            */
 
+    [_COLMAK_DH] = LAYOUT_6x3_4(
+                KC_ESC,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,                          KC_J,     KC_L,     KC_U,      KC_Y,     KC_SCLN,    KC_DEL,
+                KC_TAB,    KC_A,     KC_R,     KC_S,     KC_T,     KC_G,                          KC_M,     KC_N,     KC_E,      KC_I,     KC_O,     KC_ENTER,
+                           KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,                          KC_K,     KC_H,     KC_COMM,   KC_DOT,   KC_SLSH,
+                                       KC_BSPC,   MO(_SYMBOL),  MO(_NAVFUN), OSM(MOD_LALT), OSM(MOD_LCTL),  OSM(MOD_LSFT),    KC_SPC,  OSM(MOD_LGUI)
+
+    ),
+
     [_QWERTY] = LAYOUT_6x3_4(
             //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
                 KC_ESC,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,      KC_O,     KC_P,     KC_CAPS,
@@ -47,28 +55,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_BSPC,   MO(_SYMBOL),  MO(_NAVFUN), OSM(MOD_LALT), OSM(MOD_LCTL),  OSM(MOD_LSFT),    KC_SPC,  OSM(MOD_LGUI)
     ),
 
-    [_COLMAK_DH] = LAYOUT_6x3_4(
-            //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
-                KC_ESC,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,                          KC_J,     KC_L,     KC_U,      KC_Y,     KC_SCLN,  KC_CAPS,
-                KC_TAB,    KC_A,     KC_R,     KC_S,     KC_T,     KC_G,                          KC_M,     KC_N,     KC_E,      KC_I,     KC_O,     KC_ENTER,
-                           KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,                          KC_K,     KC_H,     KC_COMM,   KC_DOT,   KC_SLSH,
-                                       KC_BSPC,   MO(_SYMBOL),  MO(_NAVFUN), OSM(MOD_LALT), OSM(MOD_LCTL),  OSM(MOD_LSFT),    KC_SPC,  OSM(MOD_LGUI)
-
-    ),
-
     [_NAVFUN] = LAYOUT_6x3_4(
-                  KC_F1,    KC_F2,    KC_F3,       KC_F4,          KC_F5,   KC_F6,              KC_F7,       KC_F8,      KC_F9,      KC_F10,    KC_F11,   KC_F12,
-                _______,  _______,  _______, DF(_QWERTY), DF(_COLMAK_DH), _______,            _______,     KC_LEFT,    KC_DOWN,       KC_UP,  KC_RIGHT,  _______,
-                          _______,  _______,     _______,        _______, _______,            _______,      KC_END,    KC_PGDN,     KC_PGUP,   KC_HOME,
-                                     KC_DEL,   _______,     _______,        _______,            _______,     _______,    _______,     _______
+                  KC_F1, KC_F2,          KC_F3,   KC_F4,   KC_F5,   KC_F6,       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,
+                _______, DF(_QWERTY),    _______, _______, KC_CAPS, _______,     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
+                         DF(_COLMAK_DH), _______, _______, _______, _______,     _______, KC_END,  KC_PGDN, KC_PGUP, KC_HOME,
+                                         _______, _______, _______, _______,     _______, _______, _______, _______
     ),
-
 
     [_SYMBOL] = LAYOUT_6x3_4(
-                KC_GRAVE,  KC_EXLM,    KC_AT,     KC_HASH, KC_DOLLAR, KC_PERC,            KC_CIRC,     KC_AMPR,    KC_ASTR,     KC_LPRN,   KC_RPRN,  KC_BACKSLASH,
-                KC_TILDE,     KC_1,     KC_2,        KC_3,      KC_4,    KC_5,            KC_DQT,     KC_MINS,    KC_PLUS,     KC_LBRC,   KC_RBRC,  KC_PIPE,
-                              KC_6,     KC_7,        KC_8,      KC_9,    KC_0,            KC_QUOT,     KC_UNDS,    KC_EQL,      KC_LCBR,   KC_RCBR,
-                                     _______,     _______,   _______, _______,            _______,     _______,    _______,     _______
+                KC_GRAVE,  KC_EXLM,     KC_AT,  KC_HASH, KC_DOLLAR,  KC_PERC,            KC_CIRC,     KC_AMPR,   KC_ASTR, KC_LPRN,   KC_RPRN,  KC_BACKSLASH,
+                KC_TILDE,     KC_1,      KC_2,     KC_3,      KC_4,     KC_5,               KC_6,        KC_7,      KC_8,    KC_9,      KC_0,       KC_PIPE,
+                            KC_DQT,   KC_UNDS,  KC_MINS,   KC_LCBR,  KC_LBRC,            KC_RBRC,     KC_RCBR,   KC_PLUS,  KC_EQL,   KC_QUOT,
+                                     _______,     _______,   _______, _______,            _______,     _______,    _______,    _______
     ),
     /*
     [_SYMBOL] = LAYOUT_6x3_4(
